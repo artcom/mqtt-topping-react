2c974290e153b0178fdbda71adefcf650f809975
Mirko Geissler <mirko.geissler@artcom.de>
2020-06-16 17:43:27 +0200

    Merge test files

--- test/mqttProvider.test.js
+++ test/mqttProvider.test.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/first */
+jest.mock("@artcom/mqtt-topping", () => ({ unpublishRecursively: jest.fn() }))
+
+import * as topping from "@artcom/mqtt-topping"
 import React, { useContext } from "react"
 import { render } from "@testing-library/react"
 import { MqttContext, MqttProvider } from "../src"
@@ -6,17 +10,7 @@ const topic = "testTopic"
 const payload = "testPayload"
 const options = { qos: 2, stringifyJson: true, retain: true }
 
-const PublishComponent = () => {
-  useContext(MqttContext).mqtt.publish(topic, payload, options)
-  return <></>
-}
-
-const UnpublishComponent = () => {
-  useContext(MqttContext).mqtt.unpublish(topic)
-  return <></>
-}
-
-describe("MQTT", () => {
+describe("MqttProvider", () => {
   describe("Publish", () => {
     let mqtt
 
@@ -25,6 +19,11 @@ describe("MQTT", () => {
     })
 
     it("should publish given payload for given topic", () => {
+      const PublishComponent = () => {
+        useContext(MqttContext).mqtt.publish(topic, payload, options)
+        return <></>
+      }
+
       render(
         <MqttProvider mqtt={ mqtt }>
           <PublishComponent />
@@ -43,6 +42,11 @@ describe("MQTT", () => {
     })
 
     it("should unpublish a given topic", () => {
+      const UnpublishComponent = () => {
+        useContext(MqttContext).mqtt.unpublish(topic)
+        return <></>
+      }
+
       render(
         <MqttProvider mqtt={ mqtt }>
           <UnpublishComponent />
@@ -50,6 +54,31 @@ describe("MQTT", () => {
       )
 
       expect(mqtt.unpublish).toHaveBeenCalledWith(topic)
+    })
+  })
+
+  describe("UnpublishRecursively", () => {
+    let mqtt
+    let http
+
+    beforeAll(() => {
+      mqtt = "mqtt"
+      http = "http"
+    })
+
+    it("should unpublish a given topic", async () => {
+      const Component = () => {
+        useContext(MqttContext).unpublishRecursively(topic)
+        return <></>
+      }
+
+      render(
+        <MqttProvider mqtt={ mqtt } http={ http }>
+          <Component />
+        </MqttProvider>
+      )
+
+      expect(topping.unpublishRecursively).toHaveBeenCalledWith("mqtt", "http", "testTopic")
     })
   })
 })
