b7b6200d2f9bd75cf40b9b5131c4c7f84e754c59
Mirko Geissler <mirko.geissler@artcom.de>
2022-04-06 13:23:33 +0200

    2.2.0

--- src/useQueryJson.js
+++ src/useQueryJson.js
@@ -5,6 +5,7 @@ import { MqttContext } from "./mqttProvider"
 
 export default (topic) => {
   const { queryJson } = useContext(MqttContext)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const task = useCallback(() => queryJson(topic), [topic])
   return useAsyncTask(task)
 }
